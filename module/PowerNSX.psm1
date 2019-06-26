1a7970acf9f731a06465ccf83b414490b0299ced
Doug Roorda <droorda@iconicgroup.com>
2019-06-26 09:40:29 -0400

    Update PowerNSX.psm1
    I have not made this change

--- module/PowerNSX.psm1
+++ module/PowerNSX.psm1
@@ -30762,8 +30762,8 @@ function New-NsxLoadBalancerPool {
 
     process {
 
-        #Create private xml element from Current loadbalancer configuration
-        $_LoadBalancer = (Get-NsxEdge -objectId $LoadBalancer.edgeId | Get-NsxLoadBalancer).CloneNode($true)
+        #Create private xml element
+        $_LoadBalancer = $LoadBalancer.CloneNode($true)
 
         #Store the edgeId and remove it from the XML as we need to post it...
         $edgeId = $_LoadBalancer.edgeId
